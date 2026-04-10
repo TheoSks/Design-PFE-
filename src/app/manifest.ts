61637b06aa6eed79dec520c8d2c99242aaa253e4
TheoSks <gaggio880@gmail.com>
2026-04-11 01:13:05 +0200

    Fix manifest icon purpose type error

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -13,9 +13,15 @@ export default function manifest(): MetadataRoute.Manifest {
     icons: [
       {
         src: '/icon',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/icon',
         sizes: '512x512',
         type: 'image/png',
-        purpose: 'any maskable',
+        purpose: 'any',
       },
     ],
   };
